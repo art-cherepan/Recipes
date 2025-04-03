819b33054b8eac8d86a6e0c2751c22c7bd9bb1c1
Artem Cherepanov <cherepanov_aa@drom.ru>
2025-04-03 23:01:40 +0700

    [RA-05] Добавить слушателей на кнопки

--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.example.recipes
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<CategoryListFragment>(R.id.mainContainer)
+        findViewById<Button>(R.id.btnCategories)
+            .setOnClickListener {
+                supportFragmentManager.commit {
+                    replace<CategoryListFragment>(R.id.mainContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("categoryListFragment")
+                }
             }
-        }
+
+        findViewById<Button>(R.id.btnFavorites)
+            .setOnClickListener {
+                supportFragmentManager.commit {
+                    replace<FavoritesFragment>(R.id.mainContainer)
+                    setReorderingAllowed(true)
+                    addToBackStack("favoritesFragment")
+                }
+            }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
