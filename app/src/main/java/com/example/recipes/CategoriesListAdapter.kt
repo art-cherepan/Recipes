34b48fa9f32839a9ebfcf0c593283c8a45fd0d51
Artem Cherepanov <cherepanov_aa@drom.ru>
2025-04-20 22:21:35 +0700

    [RA-08] Переделать на ViewBinding

--- app/src/main/java/com/example/recipes/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recipes/CategoriesListAdapter.kt
@@ -8,28 +8,26 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recipes.databinding.ItemCategoryBinding
 import com.example.recipes.entity.Category
 
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.findViewById(R.id.image_category)
-        val titleTextView: TextView = view.findViewById(R.id.tv_title)
-        val descriptionTextView: TextView = view.findViewById(R.id.tv_description)
-    }
+    inner class ViewHolder(val binding: ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(viewGroup.context)
-        val view = inflater.inflate(R.layout.item_category, viewGroup, false)
+        val binding = ItemCategoryBinding.inflate(inflater, viewGroup, false)
 
-        return ViewHolder(view)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val category: Category = dataSet[position]
-        viewHolder.titleTextView.text = category.title
-        viewHolder.descriptionTextView.text = category.description
+
+        viewHolder.binding.tvTitle.text = category.title
+        viewHolder.binding.tvDescription.text = category.description
 
         val drawable = try {
             Drawable.createFromStream(
@@ -41,7 +39,7 @@ class CategoriesListAdapter(private val dataSet: List<Category>) :
             null
         }
 
-        viewHolder.imageView.setImageDrawable(drawable)
+        viewHolder.binding.imageCategory.setImageDrawable(drawable)
     }
 
     override fun getItemCount() = dataSet.size
