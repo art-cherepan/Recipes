0c87aa57afddeac72375f506dc935563c994b6f5
Artem Cherepanov <cherepanov_aa@drom.ru>
2025-04-30 23:34:38 +0700

    [RA-11] Добавить фрагмент RecipeFragment

---
+++ app/src/main/java/com/example/recipes/RecipeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.recipes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.recipes.databinding.FragmentRecipeBinding
+
+class RecipeFragment : Fragment() {
+    private lateinit var binding: FragmentRecipeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRecipeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
