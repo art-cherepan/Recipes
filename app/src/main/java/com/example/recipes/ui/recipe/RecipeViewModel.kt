5bba82bf73c45cf1d175e47b1e05f309d22cf880
Artem Cherepanov <cherepanov_aa@drom.ru>
2025-06-10 06:41:46 +0700

    [RA-20] Добавить свойство Recipe и portionCount во ViewModel

--- app/src/main/java/com/example/recipes/ui/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipes/ui/recipe/RecipeViewModel.kt
@@ -2,9 +2,11 @@ package com.example.recipes.ui.recipe
 
 import androidx.lifecycle.ViewModel
 import com.example.recipes.model.Ingredient
+import com.example.recipes.model.Recipe
 
 data class RecipeUiState(
-    val title: String? = null,
+    val recipe: Recipe? = null,
+    val portionCount: Int = 0,
     val ingredients: List<Ingredient> = listOf(),
     val method: List<String> = listOf(),
     val imageUrl: String? = null,
