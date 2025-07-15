db02e04f3471c2617db4fcff3c3245573d1166c9
Artem Cherepanov <cherepanov_aa@drom.ru>
2025-07-16 06:34:17 +0700

    [REF] Удалить лишний код

--- app/src/main/java/com/example/recipes/Constants.kt
+++ app/src/main/java/com/example/recipes/Constants.kt
@@ -3,5 +3,4 @@ package com.example.recipes
 object Constants {
     const val FAVORITE_RECIPES_PREFERENCES = "favorite_recipes_prefs"
     const val FAVORITE_RECIPES_KEY = "com.example.recipes.FAVORITE_RECIPES_KEY"
-    const val DEFAULT_RECIPE_IMAGE_URL = "burger-hamburger.png"
 }
--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.recipes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -41,12 +39,6 @@ class MainActivity : AppCompatActivity() {
                 setReorderingAllowed(true)
                 addToBackStack(null)
             }
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
