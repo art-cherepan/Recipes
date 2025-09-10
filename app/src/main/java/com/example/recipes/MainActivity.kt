aecac3930aaf0a306808f5591003f7fcdbdde799
Artem Cherepanov <cherepanov_aa@drom.ru>
2025-09-10 23:36:58 +0700

    [RA-33] Вывести в лог рецепты каждой категории через пулл потоков

--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -10,37 +10,37 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val threadPool: ExecutorService = Executors.newFixedThreadPool(THREAD_COUNT)
 
     companion object {
         const val GET_CATEGORIES_API_URL = "https://recipes.androidsprint.ru/api/category"
+        const val GET_RECIPES_BY_CATEGORY_URL = "https://recipes.androidsprint.ru/api/category/"
+        const val THREAD_COUNT = 10
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
-
         val thread = Thread {
             try {
-                val url = URL(GET_CATEGORIES_API_URL)
-                val connection = url.openConnection() as HttpURLConnection
-
-                connection.connect()
-
-                val responseBodyText = connection.inputStream.bufferedReader().readText()
-
-                Log.i("!!!", "Выполняю запрос на потоке: ${Thread.currentThread().name}")
-                Log.i("!!!", "Body: $responseBodyText")
+                val responseCategoryListBodyText = fetchData(GET_CATEGORIES_API_URL)
 
                 val gson = Gson()
                 val listType = object : TypeToken<List<Category>>() {}.type
-                val categoryList = gson.fromJson<List<Category>>(responseBodyText, listType)
+                val categoryList = gson.fromJson<List<Category>>(responseCategoryListBodyText, listType)
+                val categoryIds = categoryList.map { it.id }
 
-                categoryList.forEach {
-                    Log.i("!!!", "Название категории рецептов: ${it.title}")
+                categoryIds.forEach { categoryId ->
+                    threadPool.execute {
+                        val recipes = fetchData("$GET_RECIPES_BY_CATEGORY_URL$categoryId/recipes")
+
+                        Log.i("!!!", recipes)
+                    }
                 }
             } catch (e: Exception) {
                 Log.e(
@@ -63,5 +63,22 @@ class MainActivity : AppCompatActivity() {
         binding.btnFavorites.setOnClickListener {
             findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_favoriteRecipeListFragment)
         }
+    }
+
+    private fun fetchData(url: String): String {
+        val url = URL(url)
+        val connection = url.openConnection() as HttpURLConnection
+
+        try {
+            connection.connect()
+        } catch (e: Exception) {
+            Log.e(
+                "ConnectionException",
+                "Ошибка: ${e.message}",
+                e,
+            )
+        }
+
+        return connection.inputStream.bufferedReader().readText()
     }
 }
