3b45a4a62ae9ad5554ba1324ddf5d2da08d0c272
Artem Cherepanov <cherepanov_aa@drom.ru>
2025-11-08 14:34:54 +0700

    [RA-40] Добавить конвертер

--- app/src/main/java/com/example/recipes/db/AppDatabase.kt
+++ app/src/main/java/com/example/recipes/db/AppDatabase.kt
@@ -2,10 +2,20 @@ package com.example.recipes.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.recipes.model.Category
 import com.example.recipes.model.CategoryListDao
+import com.example.recipes.model.Recipe
+import com.example.recipes.model.RecipeListDao
+import com.example.recipes.model.converter.IngredientListConverter
 
-@Database(entities = [Category::class], version = 1)
+@Database(
+    entities = [Category::class, Recipe::class],
+    version = 2,
+    exportSchema = true,
+)
+@TypeConverters(IngredientListConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun categoryListDao(): CategoryListDao
+    abstract fun recipeListDao(): RecipeListDao
 }
